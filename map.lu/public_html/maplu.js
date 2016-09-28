17a710cc30529e0b3610e7c56c6e0a82fb0ea7bf
Peter Mount <peter@retep.org>
2016-09-28 16:53:33 +0100

    New nav bar using bootstrap. Now include map parameters in the url

--- map.lu/public_html/maplu.js
+++ map.lu/public_html/maplu.js
@@ -1,46 +1,232 @@
-$(document).ready(function () {
-    var a = "<a></a>", div = '<div></div>', span = "<span></span>", ul = "<ul></ul>", li = "<li></li>";
-    $('#nav').addClass("navbar navbar-inverse navbar-fixed-top")
-            .append($(div)
-                    .addClass("container")
-                    .append($(div)
-                            .addClass("navbar-header")
-                            .append($('<button></button>')
-                                    .addClass("navbar-toggle collapsed")
-                                    .attr({
-                                        "type": "button",
-                                        "data-toggle": "collapse",
-                                        "data-target": "#navbar",
-                                        "aria-expanded": "false",
-                                        "aria-controls": "navbar"
-                                    }).append($(span).addClass("sr-only").append("Toggle navigation"))
-                                    .append($(span).addClass("icon-bar"))
-                                    .append($(span).addClass("icon-bar"))
-                                    .append($(span).addClass("icon-bar"))
-                                    )
-                            .append($(a).addClass("navbar-brand").attr({'href': '/'}).append("Map.lu"))
-                            )
-                    .append($(div).attr({id: 'navbar'}).addClass("navbar-collapse collapse")
-                            .append($(ul).addClass("nav navbar-nav")
-                                    .append($(li).append($(a).attr({"href": '/about.html'}).append("About")))
-                                    .append($(li).append($(a).attr({"href": '/uk.html'}).append("UK")))
-                                    .append($(li).append($(a).attr({"href": '/world.html'}).append("World")))
-                                    )
-                            )
-                    );
+/*
+ * Map.lu application V2.0
+ * (C) Peter Mount
+ */
+(function (window) {
+    var HAS_HASHCHANGE = (function () {
+        var doc_mode = window.documentMode;
+        return ('onhashchange' in window) && (doc_mode === undefined || doc_mode > 7);
+    })();
 
-    //if ($('#mapid'))
-    {
-        var map = L.map('mapid').setView([51.505, -0.09], 15);
-        var config;
-        var ctrl = L.control.layers().addTo(map);
-        $.get("layers.json", function (d) {
-            config = d;
-            $.each(d.baseLayers, function (i, v) {
-                v.layer = L.tileLayer(v.tileLayer);
-                ctrl.addBaseLayer(v.layer,v.label);
-            });
+    L.Hash = function (map) {
+        this.onHashChange = L.Util.bind(this.onHashChange, this);
+        if (map) {
+            this.init(map);
+        }
+    };
+
+    L.Hash.parseHash = function (hash) {
+        if (hash.indexOf('#') === 0) {
+            hash = hash.substr(1);
+        }
+        var args = hash.split("/");
+        if (args.length >= 3) {
+            var zoom = parseInt(args[0], 10),
+                    lat = parseFloat(args[1]),
+                    lon = parseFloat(args[2]);
+            if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
+                return false;
+            } else {
+                var map = this.map;
+                $.each(map.meta.baseLayers, function (i, l) {
+                    map.removeLayer(l.layer);
+                });
+                $.each(map.meta.overlayLayers, function (i, l) {
+                    map.removeLayer(l.layer);
+                });
+                $.each(args, function (i, id) {
+                    if (i >= 3) {
+                        var l = map.meta.baseLayers[id];
+                        if (!l)
+                            l = map.meta.overlayLayers[id];
+                        if (l)
+                            map.addLayer(l.layer);
+                    }
+                });
+                return {
+                    center: new L.LatLng(lat, lon),
+                    zoom: zoom
+                };
+            }
+        } else {
+            return false;
+        }
+    };
+
+    L.Hash.formatHash = function (map) {
+        var center = map.getCenter(),
+                zoom = map.getZoom(),
+                precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+
+        var o = [zoom,
+            center.lat.toFixed(precision),
+            center.lng.toFixed(precision)
+        ];
+        map.eachLayer(function (l) {
+            if (l.meta && l.meta.id)
+                o.push(l.meta.id);
         });
-        console.log(baseLayers);
-    }
+        return "#" + o.join("/");
+    };
+    L.Hash.prototype = {
+        map: null,
+        lastHash: null,
+        parseHash: L.Hash.parseHash,
+        formatHash: L.Hash.formatHash,
+        init: function (map) {
+            this.map = map;
+
+            // reset the hash
+            this.lastHash = null;
+            this.onHashChange();
+
+            if (!this.isListening) {
+                this.startListening();
+            }
+        },
+        removeFrom: function (map) {
+            if (this.changeTimeout) {
+                clearTimeout(this.changeTimeout);
+            }
+
+            if (this.isListening) {
+                this.stopListening();
+            }
+
+            this.map = null;
+        },
+        onMapMove: function () {
+            // bail if we're moving the map (updating from a hash),
+            // or if the map is not yet loaded
+
+            if (this.movingMap || !this.map._loaded) {
+                return false;
+            }
+
+            var hash = this.formatHash(this.map);
+            if (this.lastHash !== hash) {
+                location.replace(hash);
+                this.lastHash = hash;
+            }
+        },
+        movingMap: false,
+        update: function () {
+            var hash = location.hash;
+            if (hash === this.lastHash) {
+                return;
+            }
+            var parsed = this.parseHash(hash);
+            if (parsed) {
+                this.movingMap = true;
+
+                this.map.setView(parsed.center, parsed.zoom);
+
+                this.movingMap = false;
+            } else {
+                this.onMapMove(this.map);
+            }
+        },
+        // defer hash change updates every 100ms
+        changeDefer: 100,
+        changeTimeout: null,
+        onHashChange: function () {
+            // throttle calls to update() so that they only happen every
+            // `changeDefer` ms
+            if (!this.changeTimeout) {
+                var that = this;
+                this.changeTimeout = setTimeout(function () {
+                    that.update();
+                    that.changeTimeout = null;
+                }, this.changeDefer);
+            }
+        },
+        isListening: false,
+        hashChangeInterval: null,
+        startListening: function () {
+            this.map.on("moveend", this.onMapMove, this);
+
+            this.map.on("baselayerchange", this.onMapMove, this);
+            this.map.on("overlayadd", this.onMapMove, this);
+            this.map.on("overlayremove", this.onMapMove, this);
+            this.map.on("layeradd", this.onMapMove, this);
+            this.map.on("layerremove", this.onMapMove, this);
+
+            if (HAS_HASHCHANGE) {
+                L.DomEvent.addListener(window, "hashchange", this.onHashChange);
+            } else {
+                clearInterval(this.hashChangeInterval);
+                this.hashChangeInterval = setInterval(this.onHashChange, 50);
+            }
+            this.isListening = true;
+        },
+        stopListening: function () {
+            this.map.off("moveend", this.onMapMove, this);
+
+            if (HAS_HASHCHANGE) {
+                L.DomEvent.removeListener(window, "hashchange", this.onHashChange);
+            } else {
+                clearInterval(this.hashChangeInterval);
+            }
+            this.isListening = false;
+        }
+    };
+    L.hash = function (map) {
+        return new L.Hash(map);
+    };
+    L.Map.prototype.addHash = function () {
+        this._hash = L.hash(this);
+    };
+    L.Map.prototype.removeHash = function () {
+        this._hash.removeFrom();
+    };
+})(window);
+
+function showCopyright() {
+    $("#copyright").modal();
+}
+
+function showMap() {
+    var map = L.map('mapid', {
+        center: [51.505, -0.09],
+        zoom: 15,
+        attributionControl: false,
+        zoomControl: true
+    });
+    // Map scale
+    L.control.scale({metric: true, imperial: true}).addTo(map);
+
+    // Attribution
+    L.control.attribution({
+        "prefix": "Map imagery ©2012-2016 Peter Mount, <a onclick=\"showCopyright();\">more information</a>"
+    }).addTo(map);
+
+    // URL shortlink
+    var hash = new L.hash(map);
+
+    var ctrl = L.control.layers().addTo(map);
+    $.get("layers.json", function (d) {
+        map.meta = d;
+
+        // Add base layers
+        $.each(d.baseLayers, function (i, v) {
+            v.layer = L.tileLayer(v.tileLayer);
+            v.layer.meta = v;
+            ctrl.addBaseLayer(v.layer, v.label);
+            // Activate first base layer
+            if (i === 0)
+                d.baseLayers[0].layer.addTo(map);
+        });
+
+        // Add overlay layers
+        $.each(d.overlayLayers, function (i, v) {
+            v.layer = L.tileLayer(v.tileLayer);
+            v.layer.meta = v;
+            ctrl.addOverlay(v.layer, v.label);
+        });
+
+    });
+}
+
+$(document).ready(function () {
+    showMap();
 });
