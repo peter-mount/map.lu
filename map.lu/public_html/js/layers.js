26d9c0832e7137c66b6c12437b901420ba613190
Peter Mount <peter@phoebe.area51.onl>
2016-10-19 08:14:02 +0100

    New layer

--- map.lu/public_html/js/layers.js
+++ map.lu/public_html/js/layers.js
@@ -7,7 +7,7 @@ var baseLayers = [
         "id": "OL",
         "label": "OSGB Land (2015)",
         "description": "Ordnance Survey Vector Map District 2015, Land/Water",
-        "tileLayer": "//map.lu/tiles/osgb15land@EPSG:900913@png/{z}/{x}/{-y}.png",
+        "tileLayer": "//map.lu/tiles/osgb15land/{z}/{x}/{-y}.png",
         "minZoom": 5,
         "maxZoom": 16
     },
@@ -15,7 +15,15 @@ var baseLayers = [
         "id": "OF",
         "label": "OSGB Full (2015)",
         "description": "Ordnance Survey Vector Map District 2015, Full theme",
-        "tileLayer": "//map.lu/tiles/osgb15full@EPSG:900913@png/{z}/{x}/{-y}.png",
+        "tileLayer": "//map.lu/tiles/osgb15full/{z}/{x}/{-y}.png",
+        "minZoom": 5,
+        "maxZoom": 16
+    },
+    {
+        "id": "OSM",
+        "label": "Open Street Map",
+        "description": "Open Street Map",
+        "tileLayer": "//map.lu/tiles/osm/{z}/{x}/{y}.png",
         "minZoom": 5,
         "maxZoom": 16
     }
@@ -25,7 +33,7 @@ var overlayLayers = [
         "id": "NR",
         "label": "OSGB Rail (2015)",
         "description": "Ordnance Survey Vector Map District 2015, Railway",
-        "tileLayer": "//map.lu/tiles/osgb15rail@EPSG:900913@png/{z}/{x}/{-y}.png",
+        "tileLayer": "//map.lu/tiles/osgb15rail/{z}/{x}/{-y}.png",
         "minZoom": 5,
         "maxZoom": 16
     }
