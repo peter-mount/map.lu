b6c0aee02a4cc935ad4b5a780e10721c2fd071ec
Peter Mount <peter@retep.org>
2018-08-06 17:44:29 +0100

    Temperature in Kelvin for raster Grib temperature layers

---
+++ stylesheets/weather/raster/temperature_k.sql
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sld
+http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd" version="1.0.0">
+  <NamedLayer>
+    <Name>Temperature K</Name>
+    <UserStyle>
+      <Title>Temperature in Kelvin</Title>
+      <FeatureTypeStyle>
+        <Rule>
+          <RasterSymbolizer>
+            <Opacity>1.0</Opacity>
+            <ColorMap type="ramp">
+              <ColorMapEntry color="#676767" quantity="245" label="-28"/>
+              <ColorMapEntry color="#888888" quantity="247" label="-26"/>
+              <ColorMapEntry color="#B0B0B0" quantity="249" label="-24"/>
+              <ColorMapEntry color="#CDCDCD" quantity="251" label="-22"/>
+              <ColorMapEntry color="#E4E4E4" quantity="253" label="-20"/>
+              <ColorMapEntry color="#773377" quantity="255" label="-18"/>
+              <ColorMapEntry color="#AA33AA" quantity="257" label="-16"/>
+              <ColorMapEntry color="#CC33CC" quantity="259" label="-14"/>
+              <ColorMapEntry color="#FF33FF" quantity="261" label="-12"/>
+              <ColorMapEntry color="#FF99FF" quantity="263" label="-10"/>
+              <ColorMapEntry color="#3300CC" quantity="265" label="-8"/>
+              <ColorMapEntry color="#3366FF" quantity="267" label="-6"/>
+              <ColorMapEntry color="#3399FF" quantity="269" label="-4"/>
+              <ColorMapEntry color="#33CCFF" quantity="271" label="-2"/>
+              <ColorMapEntry color="#33FFFF" quantity="273" label="0"/>
+              <ColorMapEntry color="#007700" quantity="275" label="2"/>
+              <ColorMapEntry color="#009900" quantity="277" label="4"/>
+              <ColorMapEntry color="#00BB00" quantity="279" label="6"/>
+              <ColorMapEntry color="#00DD00" quantity="281" label="8"/>
+              <ColorMapEntry color="#00FF00" quantity="283" label="10"/>
+              <ColorMapEntry color="#FFFF33" quantity="285" label="12"/>
+              <ColorMapEntry color="#FFEE33" quantity="287" label="14"/>
+              <ColorMapEntry color="#FFDD33" quantity="289" label="16"/>
+              <ColorMapEntry color="#FFCC33" quantity="291" label="18"/>
+              <ColorMapEntry color="#FFBB33" quantity="293" label="20"/>
+              <ColorMapEntry color="#FFAA00" quantity="295" label="22"/>
+              <ColorMapEntry color="#FF9900" quantity="297" label="24"/>
+              <ColorMapEntry color="#FF7700" quantity="299" label="26"/>
+              <ColorMapEntry color="#FF0000" quantity="301" label="28"/>
+              <ColorMapEntry color="#EE0000" quantity="303" label="30"/>
+              <ColorMapEntry color="#CC0000" quantity="305" label="32"/>
+              <ColorMapEntry color="#BB0000" quantity="307" label="34"/>
+              <ColorMapEntry color="#AA0000" quantity="309" label="36"/>
+              <ColorMapEntry color="#990000" quantity="311" label="38"/>
+              <ColorMapEntry color="#880000" quantity="313" label="40"/>
+              <ColorMapEntry color="#770000" quantity="315" label="42"/>
+              <ColorMapEntry color="#660000" quantity="317" label="44"/>
+              <ColorMapEntry color="#550000" quantity="319" label="46"/>
+              <ColorMapEntry color="#440000" quantity="321" label="48"/>
+              <ColorMapEntry color="#330000" quantity="323" label="50"/>
+              <ColorMapEntry color="#220000" quantity="325" label="52"/>
+              <ColorMapEntry color="#110000" quantity="327" label="54"/>
+              <ColorMapEntry color="#000000" quantity="329" label="56"/>
+            </ColorMap>
+          </RasterSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
